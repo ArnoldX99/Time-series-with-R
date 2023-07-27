8701970d85e102af5d573e43759f39c17e6e054d
Runqi Xu <64125777+ArnoldX99@users.noreply.github.com>
2023-07-27 20:54:04 +0800

    Delete 4_AR_model directory

--- 4_AR_model/Air passengers.R
+++
@@ -1,8 +0,0 @@
-#four year ahead forecasts for the air passenger data
-data(AirPassengers)
-AP <- AirPassengers
-AP.hw <- HoltWinters(AP, seasonal = "mult")
-plot(AP.hw)
-AP.predict <- predict(AP.hw, n.ahead = 4 * 12)
-ts.plot(AP, AP.predict, lty = 1:2)
-S
