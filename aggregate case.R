1a1ff2122597f0a9008d515ce35a8d501533e181
Runqi Xu <64125777+ArnoldX99@users.noreply.github.com>
2023-07-27 18:00:53 +0800

    Delete aggregate case.R

--- aggregate case.R
+++
@@ -1,6 +0,0 @@
-x <- data.frame(name = c('Arno','Jack','Lily','Amy'),
-                sex = c('M','M','F','F'),
-                age = c(20,30,40,50),
-                height = c(177,180,155,160))
-#Find the everage age and height for different gender
-y<-aggregate(x[,3:4],by=list(sex=x$sex),FUN = mean)
