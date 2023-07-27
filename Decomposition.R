a507de46be891fdd5fef25cc1ca8f6602d22bb30
Runqi Xu <64125777+ArnoldX99@users.noreply.github.com>
2023-07-27 18:00:28 +0800

    Delete Decomposition.R

--- Decomposition.R
+++
@@ -1,9 +0,0 @@
-#1.5.5 Decomposition in R
-Elec.decomadd <- decompose(Elec.ts)
-plot(Elec.decomadd)
-Elec.decommult <- decompose(Elec.ts, type = "mult")
-plot(Elec.decommult)
-Trend <- Elec.decomadd$trend
-Seasonal <- Elec.decomadd$seasonal
-ts.plot(cbind(Trend, Trend * Seasonal), lty = 1:2)
-#lty line type
